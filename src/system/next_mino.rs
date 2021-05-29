78efed6080e85c3b55af0154e8c643404398b13c
isso0424 <isso04240129@gmail.com>
2021-05-29 21:50:35 +0900

    test: write test for constructor

--- src/system/next_mino.rs
+++ src/system/next_mino.rs
@@ -12,11 +12,29 @@ impl NextMino {
         let mut rng = rand::thread_rng();
 
         let mut mino_list = vec![];
-        for _ in (0..max) {
+        for _ in 0..max {
             mino_list.push(minos_patterns[rng.gen_range(0..length)].clone());
         }
         Self {
             minos: Cell::new(mino_list),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn check_create_next_minos() {
+        let minos = vec![
+            Tetrimino::new(vec![vec![true; 2]; 2], (1, 1)).unwrap(),
+            Tetrimino::new(
+                vec![vec![true, false, false], vec![true, true, true]],
+                (1, 1),
+            )
+            .unwrap(),
+        ];
+        let mut next_mino_sys = NextMino::new(minos, 4);
+        assert_eq!(next_mino_sys.minos.get_mut().len(), 4);
     }
 }
