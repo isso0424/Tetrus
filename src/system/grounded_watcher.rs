ee2944d41215eb4b960b74f6598bd2e12f430e22
isso0424 <isso04240129@gmail.com>
2021-06-06 20:27:56 +0900

    feat: created grounded checker

--- src/system/grounded_watcher.rs
+++ src/system/grounded_watcher.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 use crate::types::board::Board;
+use crate::types::tetrimino::Tetrimino;
 
 pub struct GroundedWatcher<'a> {
     board: &'a Board,
@@ -8,6 +9,35 @@ pub struct GroundedWatcher<'a> {
 impl<'a> GroundedWatcher<'a> {
     pub fn new(board: &'a Board) -> Self {
         Self { board }
+    }
+
+    pub fn check(&self, mino: &Tetrimino, cursor: &(u8, u8)) -> bool {
+        let places = Self::groundable_places(mino);
+        let mut is_grounded = false;
+        places.iter().enumerate().for_each(|(x_pos, y_pos)| {
+            if *y_pos + 1 + cursor.1 as usize > mino.shape.len() {
+                is_grounded = true;
+            } else if mino.shape[*y_pos + 1 + cursor.1 as usize][x_pos] {
+                is_grounded = true;
+            }
+        });
+
+        is_grounded
+    }
+
+    fn groundable_places(mino: &Tetrimino) -> Vec<usize> {
+        let mut vx = vec![];
+        mino.shape.iter().enumerate().for_each(|(y_pos, y)| {
+            y.iter().enumerate().for_each(|(x_pos, x)| {
+                if y_pos == 0 {
+                    vx.push(0);
+                } else if *x {
+                    vx[x_pos] = y_pos;
+                }
+            })
+        });
+
+        vx
     }
 }
 
