cd9efab8c65ecb672fe9029525f697de2e45b606
isso0424 <isso04240129@gmail.com>
2021-06-07 10:13:41 +0900

    test: write test for cursor command exec

--- src/system/cursor_holder.rs
+++ src/system/cursor_holder.rs
@@ -1,10 +1,12 @@
 #![allow(dead_code)]
 use crate::types::error::CursorError;
+#[derive(Debug)]
 pub struct CursorHolder {
     cursor: (u8, u8),
     previous_command: CursorCommand,
 }
 
+#[derive(Debug)]
 pub enum CursorCommand {
     MoveLeft,
     MoveRight,
@@ -20,25 +22,61 @@ impl CursorHolder {
         }
     }
 
-    pub fn exec(&mut self, cmd: CursorCommand) -> Result<(), CursorError> {
+    pub fn exec(&mut self, cmd: CursorCommand) -> Result<&mut Self, CursorError> {
         match cmd {
             CursorCommand::MoveLeft => {
                 if self.cursor.0 == 0 {
                     Err(CursorError::OutSideUnsigned {})
                 } else {
                     self.cursor.0 -= 1;
-                    Ok(())
+                    Ok(self)
                 }
             }
             CursorCommand::MoveRight => {
                 self.cursor.0 += 1;
-                Ok(())
+                Ok(self)
             }
             CursorCommand::MoveDown => {
                 self.cursor.1 += 1;
-                Ok(())
+                Ok(self)
             }
-            CursorCommand::Nothing => Ok(()),
+            CursorCommand::Nothing => Ok(self),
         }
+    }
+
+    pub fn get_cursor(&self) -> (u8, u8) {
+        self.cursor
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn construct() {
+        let cursor_holder = CursorHolder::new((0, 0));
+        assert_eq!(cursor_holder.get_cursor(), (0, 0));
+    }
+
+    #[test]
+    fn test_move() {
+        let mut cursor_holder = CursorHolder::new((0, 0));
+        assert_eq!(cursor_holder.exec(CursorCommand::MoveLeft).is_ok(), false);
+
+        cursor_holder.exec(CursorCommand::MoveDown).unwrap();
+        assert_eq!(cursor_holder.get_cursor().0, 0);
+        assert_eq!(cursor_holder.get_cursor().1, 1);
+
+        cursor_holder.exec(CursorCommand::MoveRight).unwrap();
+        assert_eq!(cursor_holder.get_cursor().0, 1);
+        assert_eq!(cursor_holder.get_cursor().1, 1);
+
+        cursor_holder.exec(CursorCommand::MoveLeft).unwrap();
+        assert_eq!(cursor_holder.get_cursor().0, 0);
+        assert_eq!(cursor_holder.get_cursor().1, 1);
+
+        cursor_holder.exec(CursorCommand::Nothing).unwrap();
+        assert_eq!(cursor_holder.get_cursor().0, 0);
+        assert_eq!(cursor_holder.get_cursor().1, 1);
     }
 }
