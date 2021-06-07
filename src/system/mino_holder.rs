be861bfd8d435a809d721e9085bf17615b776323
isso0424 <isso04240129@gmail.com>
2021-06-07 15:53:18 +0900

    test: write test for mino holder

--- src/system/mino_holder.rs
+++ src/system/mino_holder.rs
@@ -16,3 +16,29 @@ impl<'a> MinoHolder<'a> {
         tmp
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn construct() {
+        let holder = MinoHolder::new();
+        assert_eq!(holder.holded_mino.is_none(), true);
+    }
+
+    #[test]
+    fn test_hold() {
+        let mut holder = MinoHolder::new();
+        let mino = Tetrimino::new(vec![vec![true, true], vec![true, true]], (0, 0)).unwrap();
+        let fetched = holder.hold(&mino);
+        assert_eq!(fetched.is_none(), true);
+        let fetched = holder.hold(&mino).unwrap();
+        assert_eq!(fetched.shape[0][0], true);
+        assert_eq!(fetched.shape[0][1], true);
+        assert_eq!(fetched.shape[1][0], true);
+        assert_eq!(fetched.shape[1][1], true);
+        assert_eq!(fetched.center.0, 0);
+        assert_eq!(fetched.center.1, 0);
+    }
+}
